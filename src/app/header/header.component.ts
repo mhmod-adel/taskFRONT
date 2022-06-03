a165d24ae58fa07851d852ca05543cbc7d7edfbc
Gad <ag.dev.free@gmail.com>
2022-06-03 16:29:03 +0200

    Tets

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,10 @@
+import { ThisReceiver } from "@angular/compiler";
 import { Component, OnInit } from '@angular/core';
-import { reduce } from 'rxjs';
+import { reduce, Subscription } from 'rxjs';
 import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
- 
+import { RestaurantsTypes } from "../model/shred";
+import { RestaurantsService } from "../services/resturants.service";
+
 
 @Component({
   selector: 'app-header',
@@ -9,18 +12,37 @@ import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  color:string = "whitesmoke";
-  color1:string = "whitesmoke";
+  color: string = "whitesmoke";
+  color1: string = "whitesmoke";
+  subscription: Subscription = new Subscription();
+  typesList: RestaurantsTypes[] = [];
 
-    active(){
-      this.color="tomato";
-      this.color1="whitesmoke";
-    }
-    
-  constructor() { }
+  active() {
+    this.color = "tomato";
+    this.color1 = "whitesmoke";
+  }
+
+  constructor(
+    private _Restaurants: RestaurantsService
+  ) {}
 
   ngOnInit(): void {
-     
+    this.getTypes();
   }
- 
+
+  getTypes() {
+    this.subscription.add(
+      this._Restaurants.getTypes().subscribe(res => {
+        this.typesList = res?.data;
+      })
+    )
+  }
+
+  changeType(id:any){
+    this._Restaurants.changeType.next(id);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
---
+++ src/app/model/shred.ts
@@ -0,0 +1,8 @@
+export interface RestaurantsTypes {
+  id?: number,
+  branch_type_id?: number,
+  image?: string,
+  name?: string,
+  theme_code?: string,
+  count?: number
+}
